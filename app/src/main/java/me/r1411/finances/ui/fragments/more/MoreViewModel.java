e7d3fe182d9e7838eaa5cc9a9dd49750ae8b2188
r1411 <roma00055@gmail.com>
2020-08-20 22:27:23 +0300

    Почищена третья вкладка

--- app/src/main/java/me/r1411/finances/ui/fragments/more/MoreViewModel.java
+++ app/src/main/java/me/r1411/finances/ui/fragments/more/MoreViewModel.java
@@ -1,19 +1,11 @@
 package me.r1411.finances.ui.fragments.more;
 
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class MoreViewModel extends ViewModel {
 
-    private MutableLiveData<String> mText;
 
     public MoreViewModel() {
-        mText = new MutableLiveData<>();
-        mText.setValue("This is more fragment");
-    }
 
-    public LiveData<String> getText() {
-        return mText;
     }
 }
